7728cd02ab1b9ef130b575d21deee51b9f66b7f1
roshan-yadav <mrroshan738949@gmail.com>
2025-07-16 23:57:06 +0530

    init : rotate array

---
+++ array/rotateKtime.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10};
+    int k ; 
+    cout << "Enter a number from 1 to 9 to rotate an array : ";
+    cin >> k ;
+
+    
+}
